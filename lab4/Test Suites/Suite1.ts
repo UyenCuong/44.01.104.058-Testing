115df230aee636ed5b9d2b77484c8018cde8058c
UyênCuong <nguyenthiuyencuong@gmail.com>
2020-10-07 23:00:37 +0700

    nop bai tap

---
+++ lab4/Test Suites/Suite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4a985bb8-eadb-4cc8-b5e4-de72ab2be8e5</testSuiteGuid>
+   <testCaseLink>
+      <guid>19869459-2c8c-4525-95d4-8912d7cbdcf2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_Profiles</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
